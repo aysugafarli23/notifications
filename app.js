07823e8524bdba27bdb370bded43ff135773d52d
AysuGafarli <133383250+aysugafarli23@users.noreply.github.com>
2024-04-16 23:43:44 +0400

    Version 1.1

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ const clickBtn = document.createElement('button')
 clickBtn.classList.add('clickBtn')
 clickBtn.textContent = 'Show Notifications'
 container.append(clickBtn)
-let a = Math.random() * 100
-let b = Math.random() * 100
-let c = Math.random() * 100
-let numbers = [1, 2, 3]
+let a = Math.floor(Math.random() * 256)
+let b = Math.floor(Math.random() * 256)
+let c = Math.floor(Math.random() * 256)
+
 
 clickBtn.addEventListener('click', () => {
-  const number = numbers.map(number => {
-    number
-  })
+
+
+const randomNumber = Math.floor(Math.random() * 5)
 
   const notifBtn = document.createElement('button')
   notifBtn.classList.add('notifBtn')
-  notifBtn.style.color = `rgb(${a}, ${b},${c}`
-  notifBtn.textContent = `Message ${number}`
+  notifBtn.style.color = `rgb(${a}, ${b},${c})`
+  notifBtn.textContent = `Message ${randomNumber}`
   col.append(notifBtn)
 })
+
+setTimeout(() => {
+    notifBtn.remove()
+    }, 3000)
+    
